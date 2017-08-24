221119802b6b635aa929e478fc7a244c6c29acab
Héctor González <itshgonzalez@gmail.com>
2017-08-24 12:51:16 +0200

    f

--- src/Intervalo de problemas [100-199]/Problema140.java
+++ src/Intervalo de problemas [100-199]/Problema140.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class AER_SumaDigitos {
+public class Problema140 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 		String numero = teclado.readLine();
--- src/Intervalo de problemas [100-199]/Problema168.java
+++ src/Intervalo de problemas [100-199]/Problema168.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class AER_PiezaPerdida {
+public class Problema168 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
