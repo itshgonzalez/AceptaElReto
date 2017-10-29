4de683de308a8b4aec4983cbbc640fe9dcda097f
Héctor González <itshgonzalez@gmail.com>
2017-10-29 13:09:57 +0100

    Problema 165 - Número hyperpar

--- src/Intervalo de problemas [100-199]/Problema165.java
+++ src/Intervalo de problemas [100-199]/Problema165.java
@@ -3,32 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Problema165 {
+	
+	static final BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+	static final int j = 2;
+	
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-
-		String numero = teclado.readLine();
-		int j = 2;
+		String numero = entrada.readLine();
+		
 		while (numero.charAt(0) != '-') {
 
-			boolean equals[] = new boolean[1];
+			boolean equals = true;
 
 			for (int i = 0; i < numero.length(); i++) {
-				if (numero.charAt(i) % j == 0) {
-					equals[0] = true;
-				} else if (numero.charAt(i) % j != 0) {
-					equals[0] = false;
+				if (numero.charAt(i) % j != 0) {
+					equals = false;
 					break;
 				}
 			}
 
-			if (equals[0]) {
+			if (equals) {
 				System.out.println("SI");
 			} else {
 				System.out.println("NO");
 			}
 
-			numero = teclado.readLine();
+			numero = entrada.readLine();
 		}
+		entrada.close();
 	}
 }
